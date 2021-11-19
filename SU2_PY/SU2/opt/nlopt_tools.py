e7da0934b5ca8284adba309cf08cd46176f6834c
Prateek Ranjan <pranjan2@illinois.edu>
2021-11-18 19:31:33 -0600

    updated nlopt_tools.py > needs args()

--- SU2_PY/SU2/opt/nlopt_tools.py
+++ SU2_PY/SU2/opt/nlopt_tools.py
@@ -6,6 +6,7 @@
 #  \version 7.2.0 "Blackbird"
 
 import nlopt
+import numpy as np
 from numpy import * 
 import sys
 from .. import eval as su2eval
@@ -22,18 +23,23 @@ def nlopt_optim(project,x0,xb,its,accu):
 
     """
 
-    #handle input cases
-    if x0 is None: x0 = []
-    if xb is None: xb = []
 
-    #Function handle for objective and objective sensitivity
-    #func           = obj_f
-    #fprime         = obj_df
+
+    #handle input cases
+    #if x0 is None: x0 = []
+    #if xb is None: xb = []
+
+
+
 
     # Number of design varibales
     dv_size = project.config['DEFINITION_DV']['SIZE']
-    n_dv = sum( dv_size)
+    n_dv = sum(dv_size)
     project.n_dv = n_dv
+
+    #Set the opt object definition
+    optimizer = "LD_MMA"
+    opt = nlopt.opt(nlopt.LD_MMA, n_dv)
 
     # Set the initial guess to zero
     if not x0: x0 = [0.0]*n_dv
@@ -55,14 +61,59 @@ def nlopt_optim(project,x0,xb,its,accu):
     #Scale accuracy
     eps = 1.0e-20
 
+
+    #Get the lower and upper bounds
     lb = [0.0]*n_dv
     ub = [0.0]*n_dv
     for i in range(n_dv):
-        lb[i] = xb[i][0]
-        ub[i] = xb[i][1]
+        lb[i] = np.array(xb[i][0])
+        ub[i] = np.array(xb[i][1])
+
+    #x0 = np.zeros(n_dv)
+
+    print_summary (optimizer, n_dv, obj_scale, its, accu, x0, xb)
+
+    #Define the objective function
+    opt.set_min_objective(func)
+
+    #Set bounds for the optimization problem
+    opt.set_lower_bounds(lb)
+    opt.set_upper_bounds(ub)
+
+    #x0 = np.zeros(n_dv)
+    
+    xopt = opt.optimize(x0,project)
+    
+
+def print_summary(optimizer_name, n_dv, obj_scale, its, accu, x0, xb):
+# optimizer summary
+    sys.stdout.write(optimizer_name + ' parameters:\n')
+    sys.stdout.write('Number of design variables: ' + str(n_dv) + '\n')
+    sys.stdout.write('Objective function scaling factor: ' + str(obj_scale) + '\n')
+    sys.stdout.write('Maximum number of iterations: ' + str(its) + '\n')
+    sys.stdout.write('Requested accuracy: ' + str(accu) + '\n')
+    sys.stdout.write('Initial guess for the independent variable(s): ' + str(x0) + '\n')
+    sys.stdout.write('Lower and upper bound for each independent variable: ' + str(xb) + '\n\n')
 
 
-    print('Check complete \n')    
+def func(x, grad):
+    f = 0
+    obj_list = project.obj_f(x)
+    for this_obj in obj_list:
+        f = f + this_obj
+
+    if grad.size > 0:
+        dobj_list = project.obj_df(x)
+        dobj=[0.0]*len(dobj_list[0])
+
+    for this_dobj in dobj_list:
+        idv=0
+        for this_dv_dobj in this_dobj:
+            dobj[idv] = dobj[idv]+this_dv_dobj
+            idv+=1
+        grad = array(dobj)
+
+    return f   
 
         
 
