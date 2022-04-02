586b2e6d374c24bf33683c244e646b7cda45b051
Pranjan2 <43771649+Pranjan2@users.noreply.github.com>
2022-04-02 16:37:36 -0500

    matching interface

--- SU2_CFD/src/iteration/CFluidIteration.cpp
+++ SU2_CFD/src/iteration/CFluidIteration.cpp
@@ -396,7 +396,7 @@ void CFluidIteration::MDOSolve(COutput* output, CIntegration**** integration, CG
     /*---If at the target MDO time for implicit calculations, increase the # of inner Iterations to a high value-----*/
     if (TimeIter == target_time)
     {
-      nInner_Iter = 100;
+      nInner_Iter = 10000;
     }
 
     /*--- Run a single iteration of the solver ---*/
